ed711a50fd3c88ebef1dc89dfa38ed2625307afc
awkwordMao <2624824150@qq.com>
2020-05-28 18:19:57 +0800

    Initial commit

---
+++ src/main/java/com/lzw/notifycneter/NotifyCneterApplication.java
@@ -0,0 +1,15 @@
+package com.lzw.notifycneter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyCneterApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyCneterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lzw/notifycneter/NotifyCneterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzw.notifycneter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyCneterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
